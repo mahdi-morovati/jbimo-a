c003e2c140011a0b320bf9f3caeea7b40630c935
mahdi <morovati155@gmail.com>
2022-06-25 17:35:36 +0430

    fix \App\Services\Book\BookStoreService::store

--- app/Services/Book/BookStoreService.php
+++ app/Services/Book/BookStoreService.php
@@ -5,16 +5,17 @@ namespace App\Services\Book;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Modules\Customer\Entities\Address;
 
 class BookStoreService extends BookCommonService
 {
     public function store(Request $request)
     {
-        $book = $this->repository->store($request->all());
-        $book->authors()->sync($request->authors);
-        $book->load('reviews', 'authors');
-
-        return $book;
+       return DB::transaction(function () use ($request) {
+            $book = $this->repository->store($request->all());
+            $book->authors()->sync($request->authors);
+            $book->load('reviews', 'authors');
+        }, 3);
     }
 }
